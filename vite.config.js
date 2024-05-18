b0c43e2877e9e9b2405468439b25ad5c396b5574
Kennedy N <101634005+K-njeru@users.noreply.github.com>
2024-05-18 16:44:56 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/my-vite-app/', // replace 'my-vite-app' with your repo name
+  base: '/Am-technologies/', 
 })
